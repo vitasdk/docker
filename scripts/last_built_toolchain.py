982845de23dc6380aa469bdd2a0f06a3a603fdde
scribam <scribam@users.noreply.github.com>
2023-05-23 23:34:08 +0200

    Update last_built_toolchain.py

--- scripts/last_built_toolchain.py
+++ scripts/last_built_toolchain.py
@@ -1,4 +1,3 @@
-import re
 import json
 try:
     import urllib2
@@ -6,63 +5,43 @@ except ImportError:
     # python3?
     import urllib.request as urllib2
 
-GITHUB_REPO = 'vitasdk/buildscripts'
-GITHUB = 'https://github.com'
-GITHUB_API = 'https://api.github.com'
-GITHUB_REL = GITHUB + '/' + GITHUB_REPO + '/releases'
-GITHUB_TAG = GITHUB_REL + '/tag'
+GITHUB_API_REPO = 'https://api.github.com/repos/vitasdk/buildscripts'
 TAG_FORMAT = '%(branch)s-%(os)s-v2.%(build)s'
-REGEX = re.compile('href="([^"]*)"')
 
-def find_sdk(page, os='linux'):
-    for line in page.decode('utf-8').split('\n'):
-        if os not in line or 'tar.bz2' not in line:
-            continue
-        m = REGEX.search(line)
-        if not m:
-            continue
-        return m.group(1)
-
-def pager(page, limit=20):
-    return 'limit=%(limit)d&offset=%(offset)d' % dict(limit=limit, offset=page * limit)
 
 def fetch_succeeded_tags(branch='softfp', os='linux'):
-    for page in range(5):
-        try:
-            build_url = ''.join((
-                GITHUB_API, '/repos/',
-                GITHUB_REPO,
-                '/actions/runs',
-            ))
-            req = urllib2.Request(build_url)
-            builds = json.load(urllib2.urlopen(req))
+    try:
+        build_url = ''.join((GITHUB_API_REPO, '/actions/runs', '?branch=', branch, '&conclusion=success'))
+        req = urllib2.Request(build_url)
+        builds = json.load(urllib2.urlopen(req))
+    except urllib2.HTTPError:
+        # FIXME: need to check; network error
+        return []
+    except ValueError:
+        # FIXME: need to check; json parse error
+        return []
 
-            for build in builds['workflow_runs']:
-                if build['conclusion'] != 'success':
-                    continue
-                if build['head_branch'] != branch:
-                    continue
-               
-                yield (TAG_FORMAT % dict(branch=branch,
-                                            os=os,
-                                            build=build['run_number']))
+    for build in builds['workflow_runs']:
+        yield TAG_FORMAT % dict(branch=branch, os=os, build=build['run_number'])
+
+
+def last_built_toolchain(branch='softfp', os='linux'):
+    for tag in fetch_succeeded_tags(branch=branch, os=os):
+        try:
+            release_url = ''.join((GITHUB_API_REPO, '/releases/tags/', tag))
+            req = urllib2.Request(release_url)
+            release = json.load(urllib2.urlopen(req))
         except urllib2.HTTPError:
             # FIXME: need to check; network error
             continue
         except ValueError:
             # FIXME: need to check; json parse error
             continue
-
-def last_built_toolchain(branch='softfp', os='linux'):
-    for tag in fetch_succeeded_tags(branch=branch, os=os):
-        req = urllib2.Request(GITHUB_TAG + '/' + tag)
-        try:
-            path = find_sdk(urllib2.urlopen(req).read(), os=os)
-            if not path:
-                continue
-        except urllib2.HTTPError:
+        asset_download_url = release['assets'][0]['browser_download_url']
+        if not asset_download_url:
             continue
-        return GITHUB + path
+        return asset_download_url
+
 
 if __name__ == '__main__':
     import sys
